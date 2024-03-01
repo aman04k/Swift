d65998dba53c30d121ec21064aa18ca2be8d49a8
Aman <amanprajapati7266@gmail.com>
2024-03-01 12:00:16 +0530

    add

---
+++ Tuples.swift
@@ -0,0 +1,7 @@
+// which are used to group multiple values in a single compound Value.
+// SYNTAX =>
+var TupleName = (Value1, value2,… any number of values)
+
+
+var myString = (522, "Hello")
+ print("this is value => \(myString)")
