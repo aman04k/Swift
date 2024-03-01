01a78d9d1c712240bf099561209d2ccaff8db10f
Aman <amanprajapati7266@gmail.com>
2024-03-01 15:18:28 +0530

    add

--- function.swift
+++ function.swift
@@ -5,3 +5,11 @@ func student(name: String) -> String {
 
 print(student(name: "First Program"))
 print(student(name: "About Functions"))
+
+// Calling a Function
+ func display (number: Int) -> Int{
+     let a = number
+     return a
+ }
+ print(display(number: 200))
+ print(display(number: 100))
