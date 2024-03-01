662aae366e94cecc2263153e74bef7e2998f2a2e
Aman <amanprajapati7266@gmail.com>
2024-03-01 12:19:53 +0530

    add

--- Tuples.swift
+++ Tuples.swift
@@ -11,3 +11,12 @@ var myString = (522, "Hello")
 let device = (name:"aman", age: 25)
 print(device)
 print(device.name)
+
+//Tuple elements
+var cityRatings = ("London", 7, 45.5)
+
+print("City:", cityRatings.0)
+
+print("Ratings:", cityRatings.1)
+
+print("kilometer:", cityRatings.2)
