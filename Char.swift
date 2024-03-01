db1346410ab241e23690512f2b4a93384d19513f
Aman <amanprajapati7266@gmail.com>
2024-03-01 14:17:03 +0530

    add

--- Char.swift
+++ Char.swift
@@ -2,3 +2,16 @@
 let char2: Character = "B"
 print("char1 value => \(char1)")
 print("char2 value => \(char2)")
+
+//print char in a row order
+for ch in "Hello"{
+          print(ch)
+}
+
+
+// Strings with Characters
+var constB: String = "Hello "
+let constA: Character = "G"
+
+constB.append(constA)
+print("value of constC => \(constB)")
