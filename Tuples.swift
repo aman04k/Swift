e7740ce86b0ba3ecc77e0c35b6b312e9c0e5ff55
Aman <amanprajapati7266@gmail.com>
2024-03-01 12:13:03 +0530

    add

--- Tuples.swift
+++ Tuples.swift
@@ -5,3 +5,9 @@ var TupleName = (Value1, value2,… any number of values)
 
 var myString = (522, "Hello")
  print("this is value => \(myString)")
+
+
+// name tuple
+let device = (name:"aman", age: 25)
+print(device)
+print(device.name)
