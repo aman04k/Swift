b75f6962ae7204119e93cda843e3d49320bc3d58
Aman <amanprajapati7266@gmail.com>
2024-03-01 17:04:19 +0530

    add

--- Closures.swift
+++
@@ -1,6 +0,0 @@
-//Syntax
-// closure which accepts parameters and returns a data type
-{
-   (parameters) −> return type in
-   statements
-}
---
+++ Loop.swift
@@ -0,0 +1,5 @@
+// normal loop
+var number = [1,2,3,4,5,6,78]
+for i in number{
+          print(i)
+}
