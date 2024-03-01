c459624608ddeb5ed538b57779c09986415c2053
Aman <amanprajapati7266@gmail.com>
2024-03-01 14:45:34 +0530

    add

--- Array.swift
+++ Array.swift
@@ -1,5 +1,11 @@
+//  Accessing the Array
  var arrName = [Int](arrayLiteral: 21, 55, 45)
 
 for i in arrName {
     print("\(i)")
 }
+
+// modifying the array[append mean that add the value in last index]
+ var number = [1,2,3]
+ number.append(4)
+ print(number)
