3be85723c3118aca02d63fdafcb1d43ac490f00e
Aman <amanprajapati7266@gmail.com>
2024-03-01 17:13:02 +0530

    add

--- Loop.swift
+++ Loop.swift
@@ -3,3 +3,10 @@ var number = [1,2,3,4,5,6,78]
 for i in number{
           print(i)
 }
+
+//remove element  from an array
+
+let lan = ["a", "b", "c", "d"]
+for lan in lan where lan != "b" {
+    print(lan)
+}
