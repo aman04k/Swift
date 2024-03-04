b4a08415d9f8c0507c67c4ffe3182eac7ec89c66
Aman <amanprajapati7266@gmail.com>
2024-03-04 16:59:27 +0530

    add

---
+++ BASIC/first.swift
@@ -0,0 +1,23 @@
+//FLOAT 2  NUMBER ADD
+ var array: [Float] = [2.23,1.25,3.3,2.5]
+ var sum: Float = 0.00
+ for num in array {
+     sum += num
+ }
+ print("Sum = \(sum)")
+
+ //SWIFT - DOUBLE
+ let num : Double = 23.455
+
+
+ print(num)
+
+ //sum of  two double numbers.
+
+ let num1: Double = 12.32
+ let num2: Double = 54.23
+
+ var sum: Double = 0.00
+ sum = num1 + num2
+ print("Sum of \(num1) and \(num2) = \(sum)")
+
