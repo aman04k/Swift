31373ebd840574ffee2f7c44657c0c997097febc
Aman <amanprajapati7266@gmail.com>
2024-03-04 17:59:35 +0530

    add

--- Structure/Enumerated.swift
+++ Structure/Enumerated.swift
@@ -5,7 +5,10 @@
     }
 
  //OUTPUT
- 
+
 Index 0, Name: Aman
 Index 1, Name: Ankit
 Index 2, Name: Shubham
+
+ // ENUMERATED => In Swift, the enumerated() method is a predefined method used with sequences,
+              //  such as arrays, to iterate over the elements of the sequence along with their indices
