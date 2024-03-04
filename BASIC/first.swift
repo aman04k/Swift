b47c20aceba69bd398ba32b6a29df006b16c5d69
Aman <amanprajapati7266@gmail.com>
2024-03-04 17:10:22 +0530

    add

--- BASIC/first.swift
+++ BASIC/first.swift
@@ -10,7 +10,7 @@
 
 /* It has precision of at least 15 decimal digits.
 Memory size is of 8 bytes.
-If no data type is defined, then compiler will treat it as Double  */ 
+If no data type is defined, then compiler will treat it as Double  */
 
  let num : Double = 23.455
 
@@ -25,4 +25,25 @@ If no data type is defined, then compiler will treat it as Double  */
  var sum: Double = 0.00
  sum = num1 + num2
  print("Sum of \(num1) and \(num2) = \(sum)")
+
+//SWIFT - BOOLIAN
+
+ let value0: Bool = true
+ let value : Bool = false
+
+ print(value2)
+
+ // Other method
+ let value1 = true
+ let value2 = false
+
+ //Use boolean with logical statement.
+
+ let color = true
+  if color {
+      print("My car color is red")
+  }
+  else{
+      print("my car color is not red")
+  }
 
