de36adc33f2ead9ea757c13a8cf3085d03df1ca8
Aman <amanprajapati7266@gmail.com>
2024-03-04 10:43:03 +0530

    add

---
+++ tuple/first.swift
@@ -0,0 +1,11 @@
+var aman = (45.25, 25.55) // Declaring a tuple with two elements
+let(lar, loan) = aman  // Decomposing the tuple into named constants
+let(lon, _) = aman //Decomposing the tuple and ignoring the second element using underscore
+print("value is lar \(lar)")
+print("value is lon \(lon)")
+
+/* output is =>  value is lar 45.25
+                 value is lon 45.25
+ *\
+
+
