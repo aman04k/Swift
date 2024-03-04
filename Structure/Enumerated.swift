6ce37e0bd13f821e23b39d6534499dce2507ad88
Aman <amanprajapati7266@gmail.com>
2024-03-04 18:17:17 +0530

    add

--- Structure/Enumerated.swift
+++ Structure/Enumerated.swift
@@ -14,3 +14,21 @@ Index 2, Name: Shubham
 
  // ENUMERATED => In Swift, the enumerated() method is a predefined method used with sequences,
               //  such as arrays, to iterate over the elements of the sequence along with their indices
+
+
+
+
+
+  // TYPEALIAS =>
+
+  /*
+  In Swift, a Type Alias is a way to provide a new name for an existing data type.
+  It allows you to create a new name for a data type that is already defined.
+  This can be particularly useful for making complex data types more readable and expressive,
+  or for creating more descriptive names for existing types.
+
+  */
+
+    typealias  Age = Int
+    typealias StduentID = String
+    typealias PersonTuple = (name: String, age: Int)
