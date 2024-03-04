77c161876337f3f260f5b0c4211de9907c4ff6f8
Aman <amanprajapati7266@gmail.com>
2024-03-04 17:57:07 +0530

    add

---
+++ Structure/Enumerated.swift
@@ -0,0 +1,11 @@
+    let names = ["Aman", "Ankit", "Shubham"]
+
+    for(index, name) in names.enumerated() {
+        print("Index \(index), Name: \(name)")
+    }
+
+ //OUTPUT
+ 
+Index 0, Name: Aman
+Index 1, Name: Ankit
+Index 2, Name: Shubham
